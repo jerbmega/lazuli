6d28908430fe5c34f195fd46d965810eda73d2d5
jerbmega <jerbmega@users.noreply.github.com>
2023-09-14 09:11:22 -0400

    fix: remove extensions .desktop file instead

--- config/scripts/delete_residuals.sh
+++ config/scripts/delete_residuals.sh
@@ -4,3 +4,6 @@ set -oue pipefail
 # People that need these tools can just invoke them through CLI, they don't need to appear in GNOME.
 rm -f /usr/share/applications/htop.desktop
 rm -f /usr/share/applications/nvtop.desktop
+
+# We recommend use of Extension Manger instead and Yafti installs this: we don't need this to be here.
+rm -f /usr/share/applications/org.gnome.Extensions.desktop 
