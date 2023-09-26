21e20a1715e093af0855cda300fc91bf16694fa8
jerbmega <jerbmega@users.noreply.github.com>
2023-09-26 09:04:14 -0400

    feat: replace pop-shell with forge

---
+++ config/scripts/install_forge.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+mkdir forge
+cd forge
+
+LOCATION=$(curl -s https://api.github.com/repos/forge-ext/forge/releases/latest \
+| grep "zipball_url" \
+| awk '{ print $2 }' \
+| sed 's/,$//'       \
+| sed 's/"//g' )     \
+; curl -L -o forge.zip $LOCATION
+cd $(ls -d */|head -n 1)
+
+mkdir -p temp
+cp metadata.json temp
+cp -r resources temp
+cp -r schemas temp
+cp -r config temp
+cp -r lib temp
+cp *.js temp
+cp *.css temp
+cp LICENSE temp
+mkdir -p temp/locale
+
+mkdir -p "/usr/share/gnome-shell/extensions/forge@jmmaranan.com"
+cp -r temp/* "/usr/share/gnome-shell/extensions/forge@jmmaranan.com"
+
+cd ../..
+yes | rm -rf forge
