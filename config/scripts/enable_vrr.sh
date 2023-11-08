de8edd872233617117bfeff49b8285588d399450
jerb <jsilliman@protonmail.com>
2023-11-08 08:43:26 -0500

    feat: update to Fedora 39 (#5)

--- config/scripts/enable_vrr.sh
+++ config/scripts/enable_vrr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem
 echo "MUTTER_DEBUG_FORCE_KMS_MODE=simple" >> /usr/etc/environment
-gsettings set org.gnome.mutter experimental-features "['variable-refresh-rate']"
+#gsettings set org.gnome.mutter experimental-features "['variable-refresh-rate']"
